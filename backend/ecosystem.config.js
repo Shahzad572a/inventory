dabed906137384b8e537bcb49af93aa32107e5b5
shahzad572a <shahzad572a@gmail.com>
2025-04-28 22:38:59 +0500

    renamed

--- backend/ecosystem.config.js
+++ backend/ecosystem.config.js
@@ -3,7 +3,7 @@ module.exports = {
       {
         name: "inventory-management",
         script: "npm",
-        args: "run dev",
+        args: "run start",
         env: {
           NODE_ENV: "development",
           ENV_VAR1: "environment-variable",
